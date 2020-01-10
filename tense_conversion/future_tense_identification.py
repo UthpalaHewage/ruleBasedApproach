7e86c7f923294b547604e0b84ce9a6ac087ae797
Uthpala Hewage <uthpalahewage11@gmail.com>
2020-01-10 11:15:43 +0530

    added common method to future

--- tense_conversion/future_tense_identification.py
+++ tense_conversion/future_tense_identification.py
@@ -1,8 +1,8 @@
 """Identify the sentences in future tense and filter out them"""
 
 import spacy
-import tense_conversion.Models.verb_sub_container as dict_container
 import tense_conversion.continuous_tense_conversion as continuous_tense_conversion
+import Shared.subject_root_finder as finder
 
 nlp = spacy.load('en_core_web_sm')
 
@@ -19,34 +19,21 @@ class FutureTenseIdentification(object):
         """detection of sentences in future tense and remove"""
         for i in range(len(sent_list)):
             # declare variables for later use
-            sub_index = []
             aux_index = []
-            root_verb_index = []
 
             # get the sent not marked with #-(for command det) earlier
             if sent_list[i][0] is not "#":
                 sentense = nlp(sent_list[i][0].upper() + sent_list[i][1:])
 
-                # check for the dep_=ROOT and pos_=VERB combination to get as the base root of the sentence
-                root_verb_index = [idx for idx in range(len(sentense)) if
-                                   str(sentense[idx].dep_) == "ROOT" and str(sentense[idx].pos_) == "VERB"]
-                if len(root_verb_index) != 0:
-                    # check for the (dep_=nsubj or dep_=nsubjpass)
-                    # combination out from the sent filtered out above   to get as the subject of the sentence
-                    sub_index = [idx for idx in range(len(sentense)) if
-                                 str(sentense[idx].dep_) == "nsubj" or
-                                 str(sentense[idx].dep_) == "nsubjpass"
-                                 and idx < root_verb_index[0]]
-
-                if len(sub_index) != 0:
-                    # update the created dict with root_verb_index and sub_index for later use
-                    dict_container.verb_sub_dict.update({i: [root_verb_index[0], sub_index[0]]})
+                sub_and_root = finder.subject_and_root(sentense)
+                if sub_and_root is not None:
+                    root_verb = sub_and_root[0]
+                    subject = sub_and_root[1]
                     # check for the (dep_=aux and tag_=MD combination
                     # out from the sent filtered out above  to get as the subject of the sentence
                     aux_index = [idx for idx in range(len(sentense)) if
-                                 str(sentense[idx].dep_) == "aux" and str(sentense[idx].tag_) == "MD" and sub_index[
-                                     0] < idx <
-                                 root_verb_index[0]]
+                                 str(sentense[idx].dep_) == "aux" and str(
+                                     sentense[idx].tag_) == "MD" and subject < idx < root_verb]
                     if len(aux_index) != 0:
                         for aux in aux_index:
                             # filter out the sentences with will/shall - future tense sentences
